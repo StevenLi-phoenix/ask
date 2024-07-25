2bce921305abf17702f34b8b9e82cc21c62aecbc
lishuyu <lishuyustevenli@163.com>
2024-07-25 12:52:23 -0400

    update default model to gpt-4o-mini fix miss spell need fixes with --setmodel and --setapikey option

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ ASK_GLOBAL_MODEL=os.getenv("ASK_GLOBAL_MODEL")
 if not ASK_GLOBAL_APIKEY:
     ASK_GLOBAL_APIKEY=os.getenv("OPENAI_API_KEY")
 if not ASK_GLOBAL_MODEL:
-    ASK_GLOBAL_MODEL = "gpt-4-0125-preview"
+    ASK_GLOBAL_MODEL = "gpt-4o-mini"
 if not ASK_GLOBAL_APIKEY:
     if not os.path.exists(".env"):
-        open(".env", "w").write("OPENAI_API_KEY=sk-xxxxxxxxxx\nASK_GLOBAL_MODEL=gpt-4-0125-preview\n")
+        open(".env", "w").write(f"OPENAI_API_KEY=sk-xxxxxxxxxx\nASK_GLOBAL_MODEL={ASK_GLOBAL_MODEL}\n")
     exit("API Key not found")
 tokenLimit = 128000
 
@@ -45,20 +45,22 @@ def num_tokens_from_messages(messages, model=ASK_GLOBAL_MODEL):
 # cmd line > python main.py
 # ask <text> ---- send a message to the chatbot
 # ask --setToken <token> ---- set the token for the chatbot
-# Example: ask --setToken sk-xxxxxxxxxx --model gpt-4-0125-preview What is the capital of France
+# Example: ask --setapikey sk-xxxxxxxxxx --model gpt-4-0125-preview What is the capital of France
 # Example: ask What is the capital of France
 
 def parser():
     parser = argparse.ArgumentParser(description="OpenAI Chatbot")
-    parser.add_argument("string", help="Questions", type=str, nargs='*')
     parser.add_argument("--token", "-t", help="Set the token for the chatbot", type=str, default=ASK_GLOBAL_APIKEY,
                         required=False)
     parser.add_argument("--model", "-m", help="Set the model for the chatbot", type=str, default=ASK_GLOBAL_MODEL, required=False)
     parser.add_argument("--version", "-v", help="Show the version of the chatbot", action="store_true")
     parser.add_argument("--tokenCount", help="Set the token for the chatbot", action="store_true")
-    parser.add_argument("--stream", "-s", help="Don't close chat completion and wait for more input", action="store_true")
-    parser.add_argument("--temperture", "-T", help="Set the temperture for the chatbot", type=float, default=0.7, required=False)
+    parser.add_argument("--stream", "-s", help="Don't close chat completion and wait for more input", action="store_true", required=False)
+    parser.add_argument("--temperature", "-T", help="Set the temperature for the chatbot", type=float, default=0.7, required=False)
     parser.add_argument("--tokenLimit", "-l", help="Set the token limit for the chatbot", type=int, default=tokenLimit, required=False)
+    parser.add_argument("--setapikey", help="Set the token of openai apikey", type=str, required=False)
+    parser.add_argument("--setmodel", help="Set the model for all chats", type=str, required=False)
+    parser.add_argument("string", help="Questions", type=str, nargs='*')
     return parser.parse_args()
 
 
@@ -80,7 +82,14 @@ def ask(client: openai.OpenAI, messages: list, temperature=0.7):
 
 
 def main():
+    global ASK_GLOBAL_APIKEY, ASK_GLOBAL_MODEL
     args = parser()
+    if args.setmodel or args.setapikey:
+        if args.setmodel: ASK_GLOBAL_MODEL = args.setmodel
+        if args.setapikey: ASK_GLOBAL_APIKEY = args.setapikey
+        open(".env", "w").write(f"OPENAI_API_KEY={ASK_GLOBAL_APIKEY}\nASK_GLOBAL_MODEL={ASK_GLOBAL_MODEL}\n")
+        print("Remember to upgrade tiktoken with newer model")
+        return
     if args.version:
         print("OpenAI Chatbot")
         print("Model: ", ASK_GLOBAL_MODEL)
@@ -94,7 +103,7 @@ def main():
     userchat = [
         {"role": "user", "content": " ".join(args.string)}
     ]
-    response = ask(client, userchat, args.temperture)
+    response = ask(client, userchat, args.temperature)
     userchat.append({"role": "assistant", "content": response})
     if args.stream:
         while True:
